d789162cd4f11cf9055981f098d59b1021df108b
Yoana <yoanabarbie@icloud.com>
2024-07-23 13:52:38 +0200

    feat: added the DB connection method for profile in apiCalls

--- src/views/profile/Profile.jsx
+++ src/views/profile/Profile.jsx
@@ -1,41 +1,47 @@
-import React, { useEffect, useState } from 'react';
-import { CInput } from '../../components/CInput/CInput';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { CInput } from "../../components/CInput/CInput";
+import { useNavigate } from "react-router-dom";
+import { profile } from "../../services/apiCalls";
 
 export const Profile = () => {
-    const [profileData, setProfileData] = useState({
-        name: "",
-        isActive: null,
-        createdAt: "",
-        updatedAt: "",
+  const [profileData, setProfileData] = useState({
+    name: "",
+    is_active: null,
+    created_at: "",
+    updated_at: "",
+  });
+  const passport = JSON.parse(localStorage.getItem("passport"));
+  let token;
+  const navigate = useNavigate();
 
-    })
-    const passport = JSON.parse(localStorage.getItem("passport"));
-    let token;
-    const navigate = useNavigate()
-
-    useEffect(() => {
-        if(!passport){
-            navigate('/login')
-     } else { token = passport.token; }
-    }, [
-        
-    ])
-
-    const logout = () => {
-        console.log("Byeeee")
-        localStorage.removeItem("passport")
+  useEffect(() => {
+    if (!passport) {
+      navigate("/login");
+    } 
+    else {
+      const bringProfile = async () => {
+        const response = await profile(passport.token);
+        setProfileData(response.data)
+        console.log(response)
+      }
+      bringProfile()
     }
-  
-        //if(passport)
-    return (
+  }, []);
+
+  const logout = () => {
+    console.log("Byeeee");
+    localStorage.removeItem("passport");
+  };
+
+  //if(passport)
+  return (
     <>
-    <h2>Profile info</h2>
-    <p>Name: {profileData.name}</p>
-    <p>Active:{profileData.isActive}</p>
-    <p>Created At:{profileData.createdAt}</p>
-    <p>Updated At:{profileData.updatedAt}</p>
-    <CInput type="button" value="Logout" clickFunction={logout}></CInput>
+      <h2>Profile info</h2>
+      <p>Name: {profileData.name}</p>
+      <p>Active:{profileData.is_active ? "Yes" : "No"}</p>
+      <p>Created At:{profileData.created_at}</p>
+      <p>Updated At:{profileData.updated_at}</p>
+      <CInput type="button" value="Logout" clickFunction={logout}></CInput>
     </>
-  )
-}
+  );
+};
